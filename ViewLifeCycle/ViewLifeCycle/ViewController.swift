7c9865e3afa4bc74a68bce2f05db6e6c642cc048
TiciaChance <laticia.chance@flatironschool.com>
2016-07-13 15:09:16 -0400

    tried it got it

--- ViewLifeCycle/ViewLifeCycle/ViewController.swift
+++ ViewLifeCycle/ViewLifeCycle/ViewController.swift
@@ -9,11 +9,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("viewDidLoad() called")
         view.backgroundColor = UIColor.randomColor()
+
     }
 
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         print("viewWillAppear(_:) called")
+
     }
 
     override func viewDidAppear(animated: Bool) {
